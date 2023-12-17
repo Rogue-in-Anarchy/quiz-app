114b34a0312e506f59b37e5a2b9a3cbe5383556d
Mona Lisarogue-in-anarchy <nehemiaheje@gmail.com>
2023-12-17 17:11:53 +0100

    creation of question and quiz components as well as creation of default questions and answers as json objects

---
+++ src/components/questions.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+interface QuestionProps {
+  question: string;
+  options: string[];
+  handleAnswerSelection: (selectedOption: string) => void;
+}
+
+const Question: React.FC<QuestionProps> = ({ question, options, handleAnswerSelection }) => {
+  return (
+    <div>
+      <h2>{question}</h2>
+      {options.map(option => (
+        <button key={option} onClick={() => handleAnswerSelection(option)}>
+          {option}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default Question;
